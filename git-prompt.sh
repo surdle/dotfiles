ece1b39871925088110060459942b653644179f4
Tristan Vidal <trsgot@gmail.com>
2023-05-21 12:54:36 -0500

    Add files via upload

---
+++ git-prompt.sh
@@ -0,0 +1,56 @@
+if test -f /etc/profile.d/git-sdk.sh
+then
+	TITLEPREFIX=SDK-${MSYSTEM#MINGW}
+else
+	TITLEPREFIX=$MSYSTEM
+fi
+
+if test -f ~/.config/git/git-prompt.sh
+then
+	. ~/.config/git/git-prompt.sh
+else
+	#PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
+	PS1='\[\033]0;\007\]' # set window title
+	# PS1="$PS1"'\n'                 # new line
+	# # cyan color \[\033[0;36m\]
+	# PS1="$PS1"'\[\033[0;36m\]'       # change to green
+	# PS1="$PS1"'\u@\h '             # user@host<space>
+	# PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
+	# white color \[\033[0;37m\]
+	PS1="$PS1"'\[\033[0;97m\]▲ '
+	#change to pink
+	PS1="$PS1"'\[\033[0;95m\]'       # change to pink
+	PS1="$PS1"'\w\[\033[0;37m\] '                 # current working directory
+	PS1="$PS1"'\[\033[0;97m\]'        # change color
+	if test -z "$WINELOADERNOEXEC"
+	then
+		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+		COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+		COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+		COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+		if test -f "$COMPLETION_PATH/git-prompt.sh"
+		then
+			. "$COMPLETION_PATH/git-completion.bash"
+			. "$COMPLETION_PATH/git-prompt.sh"
+			# PS1="$PS1"'\[\033[36m\]'  # change color to cyan
+			PS1="$PS1"'`__git_ps1`'   # bash function
+		fi
+	fi
+	# add space  \[\033[0m\]
+	# PS1="$PS1"'\[\033[0m\]'        # change color
+	PS1="$PS1"'\n→ '                 # new line
+	# # PS1="$PS1"'▲ '                 # prompt: always $
+fi
+
+MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
+
+# Evaluate all user-specific Bash completion scripts (if any)
+if test -z "$WINELOADERNOEXEC"
+then
+	for c in "$HOME"/bash_completion.d/*.bash
+	do
+		# Handle absence of any scripts (or the folder) gracefully
+		test ! -f "$c" ||
+		. "$c"
+	done
+fi
